a9c2f24925bdf61d587b03d0c60c87440259053c
brandonbyr4 <brandonbyrne7.bb@gmail.com>
2023-09-02 12:59:19 -0700

    Added site icon

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -37,7 +37,10 @@ export default function Header() {
     return (
         <header className="relative px-3 shadow">
             <div className="max-w-screen-xl mx-auto py-5 flex justify-between items-center">
-                <Link className="text-2xl font-semibold text-gray-900" href="/">
+                <Link className="flex items-center gap-2 text-2xl font-semibold text-gray-900" href="/">
+                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
+                    </svg>
                     SkyPulse
                 </Link>
                 <ul className="md:flex hidden items-center text-gray-900">
